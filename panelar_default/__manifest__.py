e5fab6bf09f02161fef5ae3aeab490ae89b86a8e
concagabriel <58643247+concagabriel@users.noreply.github.com>
2021-07-21 01:08:33 -0300

    Update __manifest__.py

--- panelar_default/__manifest__.py
+++ panelar_default/__manifest__.py
@@ -57,8 +57,8 @@
         'https://github.com/itpp-labs/pos-addons.git',
         'https://github.com/odoomates/odooapps.git',
         ##        'https://github.com/sistemasdehudson/sdehposaddons.git',
-        'https://github.com/OCA/manufacture.git -b 13.0'
-        'https://github.com/OCA/manufacture-reporting.git -b 13.0'
+        'https://github.com/OCA/manufacture.git -b 13.0',
+        'https://github.com/OCA/manufacture-reporting.git -b 13.0',
     ],
 
     'docker-images': [
